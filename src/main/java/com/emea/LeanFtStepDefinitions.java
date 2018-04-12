3a96f97c04fddd98d622903da1dc6d28f8cfe0ab
demo <demo@default.com>
2018-04-12 12:09:44 +0000

    Initial commit

---
+++ src/main/java/com/emea/LeanFtStepDefinitions.java
@@ -0,0 +1,19 @@
+package com.emea;
+
+import java.io.IOException;
+import com.hp.lft.report.ReportException;
+import com.hp.lft.sdk.GeneralLeanFtException;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LeanFtStepDefinitions {
+
+    public LeanFtStepDefinitions() {}
+
+    //Implementation of feature???s steps
+
+}
